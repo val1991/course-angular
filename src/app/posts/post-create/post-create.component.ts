3de6d8e42e55be0d6f820c34f9e0847d63f5af43
Valerii Davydenko <davidenko1991valerii@gmail.com>
2018-12-01 13:42:35 +0200

    init

---
+++ src/app/posts/post-create/post-create.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-post-crete',
+  templateUrl: './post-create.component.html',
+  styleUrls: ['./post-crete.component.scss']
+})
+
+export class PostCreateComponent {
+
+}
